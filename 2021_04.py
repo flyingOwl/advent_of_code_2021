f90c2dbda8859ef3cc1843831a02186f511e8553
flyingOwl <ofenfisch@googlemail.com>
2021-12-09 23:36:28 +0100

    Day 4
    Simplify solution: Calculate the score of each bingo field by matching
    the growing input list with each row/column
    
    Code needs less than 500 bytes

--- 2021_04.py
+++ 2021_04.py
@@ -2,18 +2,17 @@ import sys
 
 lines = open(sys.argv[1]).readlines()
 
-n = [ int(x) for x in lines[0].split(',') ]
+n = list(map(int,lines[0].split(',')))
 b = {}
 for i in range(len(lines) // 6):
 	b[i] = [[int(x) for x in lines[2+i*6+j].split()] for j in range(5)]
-	b[i] += [list(z) for z in zip(*b[i])]
+	b[i] += zip(*b[i])
 
-def s(n,f):
-	return n[-1] * sum(set(i for l in f[:5] for i in l) - set(n))
+def s(b):
+	m = min(next(i for i in range(len(n)+1) if all(map(n[:i].count,r))) for r in b)
+	u = sum(set(a for B in b for a in B)-set(n[:m]))
+	return m,u*n[m-1]
 
-def x(n,b,m,i=5):
-	r = [ max([len([ e for e in r if e in n[:i]]) for r in b[f]]) for f in b ]
-	return 0 if min(r) == 5 and ~m else s(n[:i], b[r.index(5)]) if 5 in r and m else x(n,b,m,i+1) or s(n[:i+1], b[r.index(4)])
-
-print("Part 1: {:d}".format(x(n,b,1)))
-print("Part 2: {:d}".format(x(n,b,0)))
+z = [s(b[i]) for i in b]
+print("Part 1: {:d}".format(min(z)[1]))
+print("Part 2: {:d}".format(max(z)[1]))
