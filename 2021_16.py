4aac40374268ebcd7302706d07b00e9398fe279d
flyingOwl <ofenfisch@googlemail.com>
2021-12-16 20:52:02 +0100

    Day 16
    Further golfing
    this also runs on python 2

--- 2021_16.py
+++ 2021_16.py
@@ -1,11 +1,14 @@
 import sys
 i=int
-m=lambda x:x[0]*m(x[1:])if x else 1
-s=lambda x:s(x[:-1])<<4|x[-1]if x else 0
+L=open(sys.argv[1]).read()[:-1]
 def p(N):
-	V=i(N[:3],2);T=i(N[3:6],2);l=T==4;b,W=i(N[6])+l,[];C=c=[22-b*4,6][l];v=i(N[7-7*l:C],2)
-	while[c-C,len(W),0][b]<v:
-		z,w,d=(0,i(N[c+1:c+5],2),5)if l else p(N[c:])
-		v*=(1^l)|i(N[c]);W+=[w];V+=z;c+=d
-	f=[sum,m,min,max,s,i.__gt__,i.__lt__,i.__eq__][T];return(V,i(f(*W)if T>4 else f(W)),c)
-for a in[0,1]:print("Part",a+1,":",p(bin(i(n:=open(sys.argv[1]).read()[:-1],16))[2:].zfill(len(n)*4))[a])
+	V=i(N[:3],2);T=i(N[3:6],2)
+	if T==4:
+		W='';c=v=6
+		while v:v=i(N[c]);W+=N[c+1:c+5];c+=5
+		W=i(W,2)
+	else:
+		b=i(N[6]);S=W=0;C=c=22-b*4
+		while[c-C,S][b]<i(N[7:C],2):z,w,d=p(N[c:]);W=[w,[W+w,W*w,min(W,w),max(W,w),0,W>w,W<w,W==w][T]][S>0];S+=1;V+=z;c+=d
+	return(V,i(W),c)
+for a in[0,1]:print("Part",a+1,":",p(bin(i(L,16))[2:].zfill(len(L)*4))[a])
